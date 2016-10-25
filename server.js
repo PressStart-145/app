20cdc1dbe5e95b31cc540ac417f85e411ff94031
Irene Ying <ireneeying@gmail.com>
2016-10-24 20:55:51 -0700

    Test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Hello World');
+	res.end('Hello World!');
 });
 
 var port = Number(process.env.PORT || 3000);
