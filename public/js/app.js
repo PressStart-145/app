c85fd3e7a7192e82e9e59939905ac859647c6a0c
Irene Ying <iying@ucsd.edu>
2016-10-27 20:11:27 -0700

    fixed sidebar toggle problem caused by previous commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,1 @@
-window.onload = function() {
-};
-
-
 $("#sidebarMenu").load('/sidebar.html');
-$(".sidebarToggleButton").click(function(e) {
-		e.preventDefault();
-		console.log($(this).attr('class'));
-		$("#sidebarDiv").toggleClass("toggled");
-});
