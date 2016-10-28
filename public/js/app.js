6c92c28ee174e908acef4a48470b9fa0aed3b817
Irene Ying <iying@ucsd.edu>
2016-10-27 19:53:50 -0700

    Updated sidebar.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
+window.onload = function() {
+};
+
+
 $("#sidebarMenu").load('/sidebar.html');
+$(".sidebarToggleButton").click(function(e) {
+		e.preventDefault();
+		console.log($(this).attr('class'));
+		$("#sidebarDiv").toggleClass("toggled");
+});
