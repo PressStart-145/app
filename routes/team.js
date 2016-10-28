c3345a44ccb813db0374090b675b638028a9fb5b
Pierre Karpov <push.default>
2016-10-27 20:49:25 -0700

    ranking page done

--- routes/team.js
+++ routes/team.js
@@ -1,5 +1,41 @@
 exports.view = function(req, res) {
     res.render('team', {
-        'castleName': "Castle 1"
+        'castleName': "Castle 1",
+        'rankings': [
+          {
+            'image' : 'PersonalAccount-01-01.png',
+            'name' : 'Irene'
+          } ,
+          {
+            'image' : 'PersonalAccount-01-01.png',
+            'name' : 'Irene'
+          } ,
+          {
+            'image' : 'PersonalAccount-01-01.png',
+            'name' : 'Pierre'
+          } ,
+          {
+            'image' : 'PersonalAccount-01-01.png',
+            'name' : 'John Snow'
+          }
+        ],
+        'achievements' : [
+          {
+            'image' : 'TeamInfo-01-01.png',
+            'name' : 'Killed a boss!'
+          } ,
+          {
+            'image' : 'TeamInfo-01-01.png',
+            'name' : 'Protected 10 towns!'
+          } ,
+          {
+            'image' : 'TeamInfo-01-01.png',
+            'name' : 'No damage in a week!'
+          } ,
+          {
+            'image' : 'TeamInfo-01-01.png',
+            'name' : '7 quests completed'
+          }
+        ]
     });
 };
