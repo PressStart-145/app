2daca3615f3c8f012552d1230b597c004600753c
Irene Ying <iying@ucsd.edu>
2016-11-01 14:16:37 -0700

    Added data to login and added build image

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
+var users = require("../users.json");
+
 $("#sidebarMenu").load('/sidebar.html');
 
 $(".backBtn").click(function() {
     e.preventDefault();
     window.history.back();
 });
+
+var addUser = function(name, username, password, email) {
+    var newUser = {
+        "name": name,
+        "username": username,
+        "password": password,
+        "email": email
+    };
+    users.users.push(newUser);
+}
