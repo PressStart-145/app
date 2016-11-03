984d48a698256e2b4c33e4d09e04e7e85096f309
Pierre Karpov <push.default>
2016-11-03 16:55:18 -0700

    account page layout done

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,8 @@ $(".backBtn").click(function(e) {
     console.log("after default");
     window.history.back();
 });
+
+$(".sidebarToggleButton").click(function(e) {
+    e.preventDefault();
+    $("#sidebarDiv").toggleClass("toggled");
+});
