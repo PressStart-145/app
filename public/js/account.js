3c5b8b8369f7bf2a42813643800c43f0123a7c58
Pierre Karpov <push.default>
2016-11-03 21:28:58 -0700

    merge

---
+++ public/js/account.js
@@ -0,0 +1,15 @@
+$("#currentQuestsBtn").click(function(e) {
+    $(this).toggleClass("active");
+    $(".doneTaskRow").addClass("hidden");
+    $(".currentTaskRow").removeClass("hidden");
+    //$("#ranks").show();
+});
+
+$("#completedQuestsBtn").click(function(e) {
+    $(this).toggleClass("active");
+    $(".doneTaskRow").removeClass("hidden");
+    $(".currentTaskRow").addClass("hidden");
+
+    console.log("hellow");
+    //$("#ranks").hide();
+});
