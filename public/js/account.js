5a60f6bbb86a57bb019b7f261243d161b0e9546e
Pierre Karpov <push.default>
2016-11-03 22:51:50 -0700

    my quest butons css fixed

--- public/js/account.js
+++ public/js/account.js
@@ -1,12 +1,14 @@
 $("#currentQuestsBtn").click(function(e) {
-    $(this).toggleClass("active");
+    $(this).addClass("selected");
+    $("#completedQuestsBtn").removeClass("selected");
     $(".doneTaskRow").addClass("hidden");
     $(".currentTaskRow").removeClass("hidden");
     //$("#ranks").show();
 });
 
 $("#completedQuestsBtn").click(function(e) {
-    $(this).toggleClass("active");
+    $(this).addClass("selected");
+    $("#currentQuestsBtn").removeClass("selected");
     $(".doneTaskRow").removeClass("hidden");
     $(".currentTaskRow").addClass("hidden");
     //$("#ranks").hide();
