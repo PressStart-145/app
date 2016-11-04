3b294bb0ddd7b8076db49ca0df5b0f75ed074b05
Irene Ying <iying@ucsd.edu>
2016-11-04 16:28:17 -0700

    Prevents urls from showing .html

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(app.router);
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/public'), {
+    extensions: ['html', 'htm']
+}));
 
 if('development' == app.get('env')) {
     app.use(express.errorHandler());
