be3e82ea6390fea9931df5b88c7be9e1ce1329cf
Pierre Karpov <push.default>
2016-11-03 19:08:21 -0700

    task come from json now

--- routes/quests.js
+++ routes/quests.js
@@ -1,6 +1,7 @@
 var data = require("../data/castles.json");
+var dataUsers = require("../data/castles.json");
 
-exports.add = function(req, res){
+exports.add = function(req, res) {
     var newQuest = {
         "title": req.body.title,
         "description": req.body.description,
@@ -14,8 +15,32 @@ exports.add = function(req, res){
 };
 
 exports.account = function(req, res) {
+    var currentUser = 'user'; //TODO lookup in dataUsers
+    var currentCastle = data.castles[0]; //TODO harcoded
+    var todoTaskList = [];
+    var doneTaskList = [];
+    var completedTask = 0;
+
+
+    for (var key in currentCastle.quests) {
+        if ((currentCastle.quests[key].takenBy === currentUser) && !currentCastle.quests[key].completed) {
+            console.log('To do: ' + currentCastle.quests[key].title);
+            todoTaskList.push(currentCastle.quests[key])
+        }
+        if ((currentCastle.quests[key].takenBy === currentUser) && currentCastle.quests[key].completed) {
+            console.log('Done: ' + currentCastle.quests[key].title);
+            doneTaskList.push(currentCastle.quests[key])
+        }
+    }
+
+    completedTask = doneTaskList.length;
+    var onlyOneCompleted = (completedTask == 1);
+
     res.render('account', {
-        'numCompleted': 5
+        'numCompleted': completedTask,
+        'currentTaskList': todoTaskList,
+        'doneTaskList': doneTaskList,
+        'onlyOneCompleted': onlyOneCompleted
     });
 };
 
