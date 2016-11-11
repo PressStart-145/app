bc7df7a1437e9af48b1d347cbb3f3a81b99237a4
Irene Ying <iying@ucsd.edu>
2016-11-10 16:31:35 -0800

    Added alert when reward if out of range

--- public/js/newQuest.js
+++ public/js/newQuest.js
@@ -7,17 +7,22 @@
 */
 
 var validateForm = function() {
+    $("#dateAlert").hide();
+    $("#levelAlert").hide();
     var deadline = document.forms["questForm"]["deadline"].value;
     var level = document.forms["questForm"]["level"].value;
     var regex = /^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/;
-    console.log(regex.test(deadline));
+    var isDeadline = true;
+    var isLevel = true;
     if (!regex.test(deadline)) {
         console.log("Invalid date format.");
         $("#dateAlert").show();
-        return false;
+        isDeadline = false;
     }
-    if (level > 10 || level < 1) {
+    if (isNaN(level) || level > 10 || level < 1) {
         console.log("Point value is out of range.");
-        return false;
+        $("#levelAlert").show();
+        isLevel = false;
     }
+    return (isDeadline && isLevel);
 }
