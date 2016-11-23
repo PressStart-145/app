508cf36a2d9eb061e393e50319a515b529ed4e1f
Pierre Karpov <push.default>
2016-11-22 21:46:48 -0800

    login oage now works with db

--- models.js
+++ models.js
@@ -2,10 +2,10 @@ var Mongoose = require('mongoose');
 
 var UserSchema = new Mongoose.Schema({
   // fields are defined here
-  "name": String,
-  "username": String,
+  "name":     String,
+  "username": {type: String,  unique : true},
   "password": String,
-  "email": String,
+  "email":    String,
   "imageURL": String
 });
 exports.User = Mongoose.model('User', UserSchema);
@@ -13,12 +13,12 @@ exports.User = Mongoose.model('User', UserSchema);
 
 
 var QuestSchema = new Mongoose.Schema({
-  "title": String,
-  "description": String,
-  "level": Number,
-  "deadline": Date,
-  "takenBy": {type: Mongoose.Schema.Types.ObjectId,  ref: 'User', default: new Mongoose.Types.ObjectId},
-  "completed": Boolean
+  "title":        String,
+  "description":  String,
+  "level":        Number,
+  "deadline":     Date,
+  "takenBy":      {type: Mongoose.Schema.Types.ObjectId,  ref: 'User', default: new Mongoose.Types.ObjectId},
+  "completed":    Boolean
 });
 exports.Quest = Mongoose.model('Quest', QuestSchema);
 
@@ -32,19 +32,19 @@ exports.Item = Mongoose.model('Item', ItemSchema);
 
 
 var GameSchema = new Mongoose.Schema({
-  "castleHealth": Number,
-  "monsterHealth": Number,
-  "items": [{type: Mongoose.Schema.Types.ObjectId,  ref: 'Item', default: new Mongoose.Types.ObjectId}]
+  "castleHealth":   Number,
+  "monsterHealth":  Number,
+  "items":          [{type: Mongoose.Schema.Types.ObjectId,  ref: 'Item', default: new Mongoose.Types.ObjectId}]
 });
 exports.Game = Mongoose.model('Game', GameSchema);
 
 
 
 var CastleSchema = new Mongoose.Schema({
-  "name": String,
-  "admin": {type: Mongoose.Schema.Types.ObjectId,  ref: 'User', default: new Mongoose.Types.ObjectId},
+  "name":     {type: String,  unique : true},
+  "admin":    {type: Mongoose.Schema.Types.ObjectId,  ref: 'User', default: new Mongoose.Types.ObjectId},
   "members": [{type: Mongoose.Schema.Types.ObjectId,  ref: 'User', default: new Mongoose.Types.ObjectId}],
-  "quests": [{type: Mongoose.Schema.Types.ObjectId,  ref: 'Quest', default: new Mongoose.Types.ObjectId}],
-  "game": {type: Mongoose.Schema.Types.ObjectId,  ref: 'Game', default: new Mongoose.Types.ObjectId}
+  "quests":  [{type: Mongoose.Schema.Types.ObjectId,  ref: 'Quest', default: new Mongoose.Types.ObjectId}],
+  "game":     {type: Mongoose.Schema.Types.ObjectId,  ref: 'Game', default: new Mongoose.Types.ObjectId}
 });
 exports.Castle = Mongoose.model('Castle', CastleSchema);
